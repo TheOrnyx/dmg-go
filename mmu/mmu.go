980b0b4850972c7b1bb1863dfe2c3960cb8c0b99
Ornyx <cthulhu345@gmail.com>
2023-12-06 15:16:47 +1300

    Added basic helper functions so I can reference them in CPU

--- mmu/mmu.go
+++ mmu/mmu.go
@@ -10,3 +10,14 @@ type MMU struct {
 	boot [256]byte // the boot/bios ram | 0x0000 -> 0x00FF
 	cart *cartridge.Cartridge
 }
+
+// ReadByte read and return the byte located at address addr
+func (mmu *MMU) ReadByte(addr uint16) byte {
+	// TODO finish this
+	return 0
+}
+
+// WriteByte write byte value data to location specified in address addr
+func (mmu *MMU) WriteByte(addr uint16, data byte)  {
+	
+}
