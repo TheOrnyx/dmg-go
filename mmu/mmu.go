8ff15ec466d48fd296404b6a8bf8c68c4ce6f6b0
Ornyx <cthulhu345@gmail.com>
2024-01-17 17:04:21 +1300

    Made various changes and made changes to read and write

--- mmu/mmu.go
+++ mmu/mmu.go
@@ -29,7 +29,8 @@ func (v *VideoRam) SwitchBank(newIndex int) error {
 
 // ReadByte read byte from active bank in vram
 func (v *VideoRam) ReadByte(addr uint16) byte {
-	return v.RAM[v.bank][addr]
+	bankAddr := addr &0x1FFF
+	return v.RAM[v.bank][bankAddr]
 }
 
 // WriteByte write given data to addr
@@ -54,10 +55,11 @@ func (w *WorkRam) SwitchBank(newIndex int) error {
 
 // ReadByte read byte from active bank in vram
 func (w *WorkRam) ReadByte(addr uint16) byte {
+	bankAddr := addr & 0x0FFF
 	if addr >= 0xC000 && addr <= 0xCFFF {
-		return w.RAM[0][addr]
+		return w.RAM[0][bankAddr]
 	} else {
-		return w.RAM[w.bank][addr]
+		return w.RAM[w.bank][bankAddr]
 	}
 }
 
@@ -78,7 +80,15 @@ type MMU struct {
 	HRAM [0x7F]byte // High ram
 	IO   [0x80]byte // I/O registers
 	OAM  [0xA0]byte // object attribute memory
-	cart *cartridge.Cartridge
+	Cart *cartridge.Cartridge
+}
+
+
+// NewMMU create and return a new MMU
+func NewMMU(cart *cartridge.Cartridge) *MMU {
+	newMMU := new(MMU)
+	newMMU.Cart = cart
+	return newMMU
 }
 
 // ReadByte read and return the byte located at address addr
@@ -86,16 +96,16 @@ func (mmu *MMU) ReadByte(addr uint16) byte {
 	// TODO finish this
 	switch {
 	case addr >= 0x0000 && addr <= 0x3FFF: // Fixed cart bank
-		return mmu.cart.MBC.ReadByte(addr)
+		return mmu.Cart.MBC.ReadByte(addr)
 
 	case addr >= 0x4000 && addr <= 0x7FFF: // cart rom bank 01 (switchable)
-		return mmu.cart.MBC.ReadByte(addr)
+		return mmu.Cart.MBC.ReadByte(addr)
 
 	case addr >= 0x8000 && addr <= 0x9FFF: // Video RAM
 		return mmu.VRAM.ReadByte(addr)
 
 	case addr >= 0xA000 && addr <= 0xBFFF: // external ram on cart
-		return mmu.cart.MBC.ReadByte(addr)
+		return mmu.Cart.MBC.ReadByte(addr)
 
 	case addr >= 0xC000 && addr <= 0xCFFF: // first work ram bank (TODO - check this is correct)
 		return mmu.WRAM.ReadByte(addr)
@@ -106,15 +116,19 @@ func (mmu *MMU) ReadByte(addr uint16) byte {
 		// Ignoring the 0xE000 -> 0xFDFF - nintendo says not allowed >:(
 
 	case addr >= 0xFE00 && addr <= 0xFE9F: // object attribute memory
-		return mmu.OAM[addr]
+		return mmu.OAM[addr&0x009F]
 
 		// ignore 0xFEA0 -> 0xFEFF - nintendo says not allowed again
 
 	case addr >= 0xFF00 && addr <= 0xFF7F: // I/O registers
-		// TODO - Implement if exists
+		return mmu.IO[addr-0xFF00] // TODO - check
+		// TODO - Implement later
 
 	case addr >= 0xFF80 && addr <= 0xFFFE: // high ram (HRAM)
-		return mmu.HRAM[addr]
+		return mmu.HRAM[addr&(0xFFFF-0xFF80)]
+
+	default:
+		
 
 	}
 	return 0
@@ -126,9 +140,12 @@ func (mmu *MMU) WriteByte(addr uint16, data byte) {
 	case addr >= 0x8000 && addr <= 0x9FFF: // Video ram
 		mmu.VRAM.WriteByte(addr, data)
 	case addr >= 0xA000 && addr <= 0xBFFF: // External ram on cart
-		mmu.cart.MBC.WriteByte(addr, data)
+		mmu.Cart.MBC.WriteByte(addr, data)
 	case addr >= 0xC000 && addr <= 0xDFFF: // work ram
 		mmu.WRAM.WriteByte(addr, data)
+	case addr >= 0xFF00 && addr <= 0xFF7F: // I/O stuff
+		mmu.IO[addr - 0xFF00] = data
+	default:
 		
 	// TODO - finish later
 	}
