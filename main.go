b5543ce77c7e43b90cb1dbf0ff94057933c32b98
Ornyx <cthulhu345@gmail.com>
2024-01-17 17:04:46 +1300

    Made a little test to be migrated to debugger

--- main.go
+++ main.go
@@ -1,10 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
-	"github.com/TheOrnyx/gameboy-golor/window"
+	"time"
+
+	"github.com/TheOrnyx/gameboy-golor/cartridge"
+	"github.com/TheOrnyx/gameboy-golor/cpu"
+	"github.com/TheOrnyx/gameboy-golor/mmu"
+	// "github.com/TheOrnyx/gameboy-golor/window"
 )
+
+type Emulator struct {
+	CPU *cpu.CPU
+	MMU *mmu.MMU
+}
+
+// NewEmulator Start a new emulator, load the rom in the given path and return the emulator instance
+func NewEmulator(romPath string) (*Emulator, error) {
+	newEmu := new(Emulator)
+	rom, err := os.ReadFile(romPath)
+	if err != nil {
+		return nil, err
+	}
+
+	cart, err := cartridge.LoadROM(rom)
+	if err != nil {
+		return nil, err
+	}
+
+	newEmu.MMU = mmu.NewMMU(cart)
+	
+	newEmu.CPU, _ = cpu.NewCPU(newEmu.MMU)
+
+	return newEmu, nil
+}
+
+// PrintDebugInfo print debug info about the emulator
+func (e *Emulator) PrintDebugInfo()  {
+	DebugLog.Println("Rom name:", e.MMU.Cart.Title)
+}
 
 const UsingSDL = true
 const WinScalar = 4 //the scalar used to scale up the gbc screen
@@ -17,7 +53,26 @@ var FatalLog = log.New(os.Stdout, "[FaTAL] ", log.LstdFlags)
 
 func main() {
 	InfoLog.Println("Starting...")
-	window.StartSDLWindowSystem(WinWidth, WinHeight)
+	// window.StartSDLWindowSystem(WinWidth, WinHeight)
+	emulator, err := NewEmulator("./Data/Roms/blargg-test-roms/cpu_instrs/cpu_instrs.gb")
+	if err != nil {
+		FatalLog.Fatal("Error making new emulator:", err)
+	}
+	emulator.CPU.ResetDebug()
+	emulator.PrintDebugInfo()
+
+	var running bool = true
+	for running {
+		emulator.CPU.Step()
+		DebugLog.Println(emulator.CPU)
+		if emulator.MMU.ReadByte(0xff02) != 0 {
+			char := emulator.MMU.ReadByte(0xff01)
+			fmt.Println("Found write")
+			fmt.Printf("%v", char)
+			// emulator.MMU.WriteByte(0xff02, 0)
+		}
+		time.Sleep(time.Second/2)
+	}
 	
 	InfoLog.Println("Program finished, exiting...")
 }
