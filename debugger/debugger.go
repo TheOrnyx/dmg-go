008c2fd0c29d8f04f6a7183317220924522bd891
Ornyx <cthulhu345@gmail.com>
2024-01-24 10:36:05 +1300

    Started making a basic debugger for checking emulator state

--- debugger/debugger.go
+++ debugger/debugger.go
@@ -1,1 +1,46 @@
 package debugger
+
+import (
+	"fmt"
+
+	"github.com/TheOrnyx/gameboy-golor/emulator"
+)
+
+type Debugger struct {
+	Emu *emulator.Emulator
+}
+
+// DebugEmulator run and debug an emulator
+func DebugEmulator(emu *emulator.Emulator)  {
+	emu.CPU.ResetDebug()
+	fmt.Println(emu.MMU.Cart.ROM)
+	fmt.Println("Beginning debug...")
+	fmt.Println(emu.DebugInfo())
+	running := true
+	// debugger := Debugger{emu}
+	
+	for running {
+		fmt.Println("---------------------------------------------")
+		fmt.Println("")
+		// serialWritten, data := debugger.checkSerialLink()
+		// if serialWritten {
+		// 	fmt.Println("Found serial data write")
+		// 	fmt.Printf("%v", data)
+		// }
+		emu.CPU.Step()
+		fmt.Printf("%v\n", emu.CPU)
+		fmt.Scanln()
+	}
+}
+
+// checkSerialLink check whether data has been sent to the serial link and return it and the status
+func (d *Debugger) checkSerialLink() (bool, byte) {
+	if d.Emu.MMU.ReadByte(0xFF02) == 0x81 {
+		char := d.Emu.MMU.ReadByte(0xFF01)
+		d.Emu.MMU.WriteByte(0xFF02, 0)
+		return true, char
+		// fmt.Println("Found write")
+		// fmt.Printf("%v", char)
+	}
+	return false, 0
+}
